5fcc0341527ea6a55b3051746aed949c9dd6e753
Kent Riemondy <kent.riemondy@gmail.com>
2018-05-07 13:17:48 -0600

    update for novaseq parsing

--- pipeline/snakecharmer.sh
+++ pipeline/snakecharmer.sh
@@ -4,10 +4,13 @@
 #BSUB -e logs/10x_%J.err
 #BSUB -R "select[mem>4] rusage[mem=4] " 
 #BSUB -q normal
+#BSUB -m "compute16"
 
 set -o nounset -o pipefail -o errexit -x
 
-args=' -q rna -o {log}.out -e {log}.err -J {params.job_name} -R " {params.memory} span[hosts=1] " -n {threads}'
+args=' -q rna -o {log}.out -e {log}.err -J {params.job_name} -R "
+{params.memory} span[hosts=1] " -n {threads} -m "compute03 compute07
+    compute16 " '
 
 snakemake --drmaa "$args" --snakefile Snakefile --jobs 3 \
-  --latency-wait 100 --rerun-incomplete  --configfile config.yaml 
+  --latency-wait 50 --rerun-incomplete  --configfile config_jordan.yaml 
