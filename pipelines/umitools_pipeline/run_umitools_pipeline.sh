a1e306f6bdb503acea24cb6f38dcae566173d12d
Kent Riemondy <kent.riemondy@gmail.com>
2018-06-15 12:21:59 -0600

    fix versions for modules and rename output logs

--- pipelines/umitools_pipeline/run_umitools_pipeline.sh
+++ pipelines/umitools_pipeline/run_umitools_pipeline.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-#BSUB -J 10xscRNA 
-#BSUB -o logs/10x_%J.out
-#BSUB -e logs/10x_%J.err
+#BSUB -J scRNA 
+#BSUB -o logs/scrna_%J.out
+#BSUB -e logs/scrna_%J.err
 #BSUB -R "select[mem>4] rusage[mem=4] " 
 
 set -o nounset -o pipefail -o errexit -x
 
-args=' -q rna -o {log}.out -e {log}.err -J {params.job_name} -R " {params.memory} span[hosts=1] " -n {threads} '
+args=' -q normal -o {log}.out -e {log}.err -J {params.job_name} -R " {params.memory} span[hosts=1] " -n {threads} '
 
 #### load necessary programs ####
 
@@ -16,12 +16,12 @@ args=' -q rna -o {log}.out -e {log}.err -J {params.job_name} -R " {params.memory
 # load modules
 . /usr/share/Modules/init/bash
 module load modules modules-init modules-python
-module load fastqc/0.10.1
+module load fastqc
 module load samtools
 module load star 
 
 # featureCounts >= 1.6.0
-# umi_tools >= 0.5.3
+# umi_tools >= 0.5.1
 
 snakemake --drmaa "$args" \
   --snakefile umitools.snake \
